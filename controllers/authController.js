f05867a0c13131fc5c6e0ea28721405527bee156
murat-yildirim@hotmail.com.tr <murat-yildirim@hotmail.com.tr>
2025-07-28 11:19:38 +0300

    update

---
+++ controllers/authController.js
@@ -0,0 +1,25 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+let users = [] //burada gecici user tutuyoruz db kullanmadigimiz icin
+
+exports.signup = async (req, res) => {
+    const {username, password} = req.body
+    const encodedPassword = await bcrypt.hash(password, 10)
+    users.push({username, password: encodedPassword})
+    res.json({message: 'User created.'})
+}
+
+exports.login = async (req, res) => {
+    const {username, password} = req.body
+    const user = users.find(u => u.username === username)
+    if(!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({message: 'Kullanici adi veya sifre hatali'})
+    }
+    const token = jwt.sign({username}, process.env.JWT_SECRET, {expiresIn: '1h'})
+    res.json({token})
+}
+
+exports.profile = (req, res) => {
+    res.json({user: req.user})
+}
---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { signup, login, profile } = require("../controllers/authController");
+const { verifyToken } = require("../middleware/authMiddleware");
+
+const router = express.Router()
+
+router.post('/signup', signup)
+router.post('/login', login)
+router.get('/profile', verifyToken, profile)
+
+module.exports = router
