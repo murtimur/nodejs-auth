4d4bdaa7cdc0e9b73c763535e41a1c44b6c2c80b
Murat YILDIRIM <46280331+murtimur@users.noreply.github.com>
2025-07-30 11:48:45 +0300

    Update

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,7 +14,7 @@ exports.login = async (req, res) => {
     const {username, password} = req.body
     const user = users.find(u => u.username === username)
     if(!user || !(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({message: 'Kullanici adi veya sifre hatali'})
+        return res.status(401).json({message: 'Username or password is incorrect'})
     }
     const token = jwt.sign({username}, process.env.JWT_SECRET, {expiresIn: '1h'})
     res.json({token})
